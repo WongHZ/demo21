1a29eabfa337bec5662b2c3fd916ccc97a395028
HUAN <2277012678@qq.com>
2022-08-20 21:20:17 +0800

    demo21Application

--- demo21/src/main/java/com/huan/demo21/entity/Message.java
+++ demo21/src/main/java/com/huan/demo21/entity/Message.java
@@ -1,10 +1,12 @@
 package com.huan.demo21.entity;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Data
 public class Message {
     private String name;
     private String content;
